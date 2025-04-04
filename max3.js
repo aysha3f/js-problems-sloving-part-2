d96d5b29c69e8f2acfaccd965479b9d74a585a0b
aysha3f <tahiaysha220@gmail.com>
2025-04-05 02:37:11 +0600

    max3

---
+++ max3.js
@@ -0,0 +1,21 @@
+const jim = 256;
+const tim = 389;
+const kim = 168;
+
+if(jim > tim && jim > kim){
+    console.log('Jim is the ultimate boss')
+}
+else if(tim > jim && tim > kim){
+    console.log('Tim is the boss')
+}
+else{
+console.log('Kim is the Kardashians boss ')
+}
+
+function maxOfThree (num1, num2, num3){
+ //do it yourself
+}
+
+
+const max = Math.max(12, 1, 56, 67,8787,687,43,345,35434,)
+console.log('max is using Math.max', max);
