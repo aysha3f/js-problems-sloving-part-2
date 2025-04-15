3c3a1a3206b923550ef4ae8b4ce4835cc58f3b90
aysha3f <tahiaysha220@gmail.com>
2025-04-16 02:14:39 +0600

    task -5 complete

--- p.js
+++ p.js
@@ -77,13 +77,29 @@ return total;
 //    const getTotal = findAveragePhonePrice(6)
 //    const getTotal = findAveragePhonePrice(phones.length)
 //    console.log(getTotal);
-// Task -5: (Hard)
-// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
 
-//  const employees = [
-//             { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
-//             { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
-//             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
-//             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
-//         ];
+// Task -5: (Hard)
+// For each employee their current salary is calculated by multiplying yearly increment
+//  with experience then adding the result to the starting salary. Now calculate is the
+//  total salary has to be provided by the company in a month.
+
+ const employees = [
+           { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+           { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+          { name: "shohel", experience: 0, starting: 29000, increment: 4000 },     
+        
+        ];
+
+        function calculateTotalSalary (employees){
+            let sum = 0;
+            for(const employee of employees){
+                let currentSalary = employee.starting + (employee.experience * employee.increment);
+                sum = sum + currentSalary;
+            }
+            const monthlySalary = sum / 12;
+            return monthlySalary;
+        }
+        const total = calculateTotalSalary(employees);
+        console.log(total);
 
