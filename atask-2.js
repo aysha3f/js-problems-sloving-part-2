060b3539ac415a3f96429f1e8cb8787bb64a6cf1
aysha3f <tahiaysha220@gmail.com>
2025-04-18 20:55:06 +0600

    task-2 again complete

--- atask-2.js
+++ atask-2.js
@@ -1,17 +1,16 @@
 // node atask-2.js
 
 function sendNotification(email) {
-
-  if(email.indexOf('@') === -1 ){
+if(email.indexOf('@') === -1){
     return "Invalid Email"
-  }
-
-let parts = email.split("@")
-    let userName = parts[0];
-    let domainName = parts[1];
-    return `${userName} sent you an email from ${domainName}`;
+}
+ let parts = email.split('@');
+ let userName = parts[0];
+ let domainName = parts[1];
+ return `${userName} sent you an email from ${domainName}`;
     
 }
-console.log(sendNotification("zihad@gmail.com"))
+console.log(sendNotification("nadim.naem5@outlook.com"))
+console.log(sendNotification("nadim.naem5outlook.com"))
 
 
