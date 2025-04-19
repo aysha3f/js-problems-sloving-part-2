ba47084f9795f1000143b0a24af400a96ddbef40
aysha3f <tahiaysha220@gmail.com>
2025-04-19 22:55:28 +0600

    .

--- atask-3.js
+++ atask-3.js
@@ -1,13 +1,6 @@
 // node atask-3.js
 
 function checkDigitsInName(name) {
-    if(typeof name !== 'string'){
-        return "Invalid Input"
-    }
     
-
-let n = /\d/.test(name)
-
-    return n;
 }
-console.log(checkDigitsInName(34))
+
