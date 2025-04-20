d3f5229abcee953248a55605e7276af291bad31d
aysha3f <tahiaysha220@gmail.com>
2025-04-20 23:57:58 +0600

    .

--- attask-4.js
+++ attask-4.js
@@ -1,23 +1,7 @@
 // node attask-4.js
 
 function calculateFinalScore(obj) {
-    typeof name = string 
-    typeof testScore = number ( testScore <= 50) 
-    typeof schoolGrade = number ( schoolGrade <=30) 
-    typeof  isFFamily = boolean
-    
-    if(typeof obj !== "string"  || obj !== "number" || obj !== "array"){
-        return "Invalid Input"
-      }
-
-      if(isFFamily === true){
-        let finalScore =  student.testScore + student.schoolGrade + 20;
-        console.log(finalScore);
-      }
-      else if(finalScore >= 80){
-    return "true"
-      }
-      else (finalScore < 80){
-        return "false"
-      }
+  if(typeof obj !== "object" || obj === null || Array .isArray (obj)){
+    return"Invalid Input"
+  }
 }
