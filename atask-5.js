c6230d75e6c6e96a0a26b5a0343537a35c6ee5ad
aysha3f <tahiaysha220@gmail.com>
2025-04-21 03:57:36 +0600

    assignment -4 problem 5 completed

--- atask-5.js
+++ atask-5.js
@@ -1,11 +1,11 @@
 // node atask-5.js
 
 function  waitingTime(waitingTimes  , serialNumber) {
-  if(!Array.isArray (waitingTimes)){
+  if(!Array.isArray(waitingTimes)){
     return "Invalid Input"
   }
 
-  for(const time of waitingTimes){
+  for (const time of waitingTimes){
     if(typeof time !== "number"){
         return "Invalid Input"
     }
@@ -14,7 +14,7 @@ function  waitingTime(waitingTimes  , serialNumber) {
   if(typeof serialNumber !== "number"){
     return "Invalid Input"
   }
-
+  
   let totalTime = 0;
   for(const time of waitingTimes){
     totalTime = totalTime + time;
@@ -27,8 +27,16 @@ function  waitingTime(waitingTimes  , serialNumber) {
   if(peopleRemaining <= 0){
     return 0;
   }
-  let waitingTime = peopleRemaining *averageTime;
+  let waitingTime = averageTime * peopleRemaining;
   return waitingTime;
-
 }
-console.log(waitingTime([ 3, 5, 7, 11, 6 ], 10))
+// console.log(waitingTime([ 3, 5, 7, 11, 6 ], 10));
+// console.log(waitingTime([13, 2], 6));
+// console.log(waitingTime([13, 2, 6, 7, 10], 6));
+// console.log(waitingTime([6], 4))
+// console.log(waitingTime(7 , 10))
+// console.log(waitingTime("[6,2]", 9))
+console.log(waitingTime([7, 8, 3, 4, 5], "9"))
+
+
+// node atask-5.js
